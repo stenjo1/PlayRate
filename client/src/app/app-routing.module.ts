94191dd0f846367a930e8892d6be0bcdc2319174
Tihomir <tstojkovic99@hotmail.com>
2022-12-01 16:58:18 +0100

    some app routes added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GamePageComponent } from './components/game-page/game-page.component';
+import { SignUpInComponent } from './components/sign-up-in/sign-up-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:SignUpInComponent},
+  {path:'gamepage',component:GamePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
