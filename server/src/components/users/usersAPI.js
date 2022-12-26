36fe33aa51475e2259860294ac1741e8fee85faf
MinjaP <minjap@twodesperados.com>
2022-12-27 00:41:57 +0100

    usersAPI fix

--- server/src/components/users/usersAPI.js
+++ server/src/components/users/usersAPI.js
@@ -13,6 +13,7 @@ router.put("/addPlayingGame",controller.addPlayingGame);
 router.put("/addBacklogGame",controller.addBacklogGame);
 router.get('/:username/posts', controller.getPostsForUser);
 
+router.get('/getUser/:username', controller.getUserByUsername);
 router.get("/games/:username",controller.getGames);
 
 router.delete("/removeFinishedGame",auth.isAuthenticated,controller.removeFinishedGame);
@@ -22,7 +23,6 @@ router.delete("/removeBacklogGame",auth.isAuthenticated,controller.removeBacklog
 router.put("/addPost",auth.isAuthenticated,controller.addPost);
 router.delete("/removePost",auth.isAuthenticated,controller.removePost);
 
-router.get('/getUser/:username', controller.getUserByUsername);
 
 
 module.exports = router;
